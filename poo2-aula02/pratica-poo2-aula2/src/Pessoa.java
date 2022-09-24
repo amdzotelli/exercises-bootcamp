1a0997b4138474bc8c70ef2166f80668c3247afe
amdzotelli <amanda.zotelli@mercadolivre.com>
2022-09-23 22:15:12 -0300

    exercício 2 - construtores

--- poo2-aula02/pratica-poo2-aula2/src/Pessoa.java
+++ poo2-aula02/pratica-poo2-aula2/src/Pessoa.java
@@ -4,4 +4,26 @@ public class Pessoa {
     String ID;
     double peso;
     double altura;
+
+    public Pessoa() {}
+
+    public Pessoa(String nome, int idade, String ID) {
+        this.nome = nome;
+        this.idade = idade;
+        this.ID = ID;
+    }
+
+    public Pessoa(
+    String nome,
+    int idade,
+    String ID,
+    double peso,
+    double altura
+    ) {
+        this.nome = nome;
+        this.idade = idade;
+        this.ID = ID;
+        this.peso = peso;
+        this.altura = altura;
+    }
 }
