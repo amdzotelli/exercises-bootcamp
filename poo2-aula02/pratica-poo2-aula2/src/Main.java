768262402e625ffd8416564ba7233e699cffb201
amdzotelli <amanda.zotelli@mercadolivre.com>
2022-09-23 23:23:25 -0300

    exercício 4 - implementando pessoa

--- poo2-aula02/pratica-poo2-aula2/src/Main.java
+++ poo2-aula02/pratica-poo2-aula2/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa alberto = new Pessoa();
+
+        alberto.nome = "Alberto";
+        alberto.ID = "a1b2c3";
+        alberto.altura = 1.72;
+        alberto.peso = 95;
+        alberto.idade = 32;
+
+        Pessoa juliana = new Pessoa("Juliana", 23, "aaa2e3");
+
+        Pessoa magali = new Pessoa("Magali", 19, "1e2b3", 65, 1.65);
+
+        System.out.println(alberto.nome + " " + juliana.idade + " " + magali.peso);
     }
 }
